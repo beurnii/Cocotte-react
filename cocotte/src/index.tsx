0f7302654629b58a8d7bf3a0047b6ea148de65af
Bernard <bern97@hotmail.ca>
2019-07-16 23:08:50 -0400

    eslint lint et router

--- cocotte/src/index.tsx
+++ cocotte/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App-Component/App';
 import * as serviceWorker from './serviceWorker';
+import {Router, Route, Switch} from 'react-router';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
